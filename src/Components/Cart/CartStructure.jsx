25f695eb0ebfefc338132f394947887487ad6f9a
ZMichaelaK <korn_miku@hotmail.com>
2024-03-07 10:41:37 +0000

    incremental and decremental

--- src/Components/Cart/CartStructure.jsx
+++ src/Components/Cart/CartStructure.jsx
@@ -3,21 +3,29 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function CartStructure(props) {
-  const [quantity, setQuantity] = useState(0);
+ const [quantity, setQuantity] = useState(0);
 
+ const incrementQuantity = () => {
+    setQuantity(prevQuantity => prevQuantity + 1);
+ };
 
-
-  const params = useParams();
-    return ( 
-      <div className="card-body" style={{textAlign: "center", backgroundColor: "lightblue", borderRadius: "10px", padding: "10px", fontWeight: "bold"}}>
-        <p className="card-title">{props.itemName}</p>
-        <p className="card-title">£{props.price}</p>
-        <p className="card-title">Quantity x{props.quantity}</p>
-        <button  style={{marginTop: "5px"}}  className="btn btn-success">Quantity +1</button>
-        <button  style={{marginLeft: "10px", marginTop: "5px"}} className="btn btn-danger ">Quantity -1</button>
-      </div>
-     );
+ const decrementQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(prevQuantity => prevQuantity - 1);
     }
+ };
 
+ const params = useParams();
+
+ return (
+    <div className="card-body" style={{textAlign: "center", backgroundColor: "lightblue", borderRadius: "10px", padding: "10px", fontWeight: "bold"}}>
+      <p className="card-title">{props.itemName}</p>
+      <p className="card-title">£{props.price}</p>
+      <p className="card-title">Quantity x{quantity}</p>
+      <button onClick={incrementQuantity} style={{marginTop: "5px"}} className="btn btn-success">Quantity +1</button>
+      <button onClick={decrementQuantity} style={{marginLeft: "10px", marginTop: "5px"}} className="btn btn-danger ">Quantity -1</button>
+    </div>
+ );
+}
 
 export default CartStructure;
